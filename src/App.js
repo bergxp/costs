239e07f6483cab5b8bf433338021a32ad71f7d53
Berg <berg215@hotmail.com>
2023-02-22 08:24:21 -0300

    Atualização

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './componentes/pages/Home.js';
+import Company from './componentes/pages/Company.js';
+import Contact from './componentes/pages/Contact.js';
+import NewProject from './componentes/pages/NewProject';
+import Container from './componentes/layout/Container.js';
+import Navbar from './componentes/layout/Navbar'
+import Footer from './componentes/layout/Footer'
+import Projects from './componentes/pages/Projects.js';
+import Project from './componentes/pages/Project.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+     <Navbar/>
+      <Container customClass='min-height'>
+      <Routes>
+        
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/projects' element={<Projects/>}></Route>
+        <Route path='/company' element={<Company/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/newproject' element={<NewProject/>}></Route>
+        <Route path='/project/:id' element={<Project/>}></Route>
+      </Routes>
+      </Container>
+      <Footer/>
+    </Router>
   );
 }
 
---
+++ src/componentes/pages/NewProject.js
@@ -0,0 +1,32 @@
+import {useNavigate} from 'react-router-dom'
+import ProjectForm from '../project/ProjectForm'
+import styles from './NewProject.module.css'
+function NewProject(){
+  const navigate = useNavigate()
+  function creatPost(project){
+    //Initialize cost and services
+    project.cost = 0
+    project.services =[]
+    const url = "http://localhost:5000/projects"
+    const metodo = {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(project),
+    }
+    fetch(url,metodo).then((resp) => resp.json())
+    .then((data) => {
+      //redirect
+      navigate('/projects',{state: {message: "Projeto criado com sucesso"}})
+    }).catch((err) => alert(err))
+  }
+  return(
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar o serviço</p>
+      <ProjectForm handleSubmit={creatPost} btnText='Criar Projeto' />
+    </div>
+  )
+}
+export default NewProject
